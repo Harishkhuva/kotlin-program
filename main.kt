0471e36e8b0fb86529b27e811787252e0dee6930
Harishkhuva <108509301+Harishkhuva@users.noreply.github.com>
2025-07-17 13:12:40 +0530

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,64 @@
+fun main() {
+//    println("Enter the distance in kilometer : ")
+//    val km = readln()!!.toInt()
+//    val feet = km*3280.84
+//    val meter = km*1000
+//    val inch = km*39370.1
+//    val cm = km*100000
+//    println("Distance in Feet : $feet")
+//    println("Distance in Meter : $meter")
+//    println("Distance in Inch : $inch")
+//    println("Distance in Centimeter : $cm")
+
+    // Reverse String using function
+//    println("Enter the string you want reverse : ")
+//    val targetstring = readln()
+//    val reverse = targetstring.reversed()
+//    println("Your Target string is : $targetstring")
+//    println("Your Reverse string is : $reverse")
+
+    // reverse string using for loop
+//    println("Enter the string you want reverse : ")
+//    val targetstring = readln()
+//    var reverse = ""
+//    for(i in targetstring.length - 1 downTo 0){
+//        reverse += targetstring[i]
+//    }
+//
+//    println(targetstring.length)
+
+//    println("Enter the number you to add the numbers")
+//    val num = readln().toInt()
+//    val arr = IntArray(num)
+//
+//    for(i in 0 until num){
+//        println("Enter the number ${i+1}")
+//        val input = readln()!!.toInt()
+//        arr[i] = input
+//    }
+//    println("The number in array is ${arr.joinToString(", ") }")
+//    val sum = arr.sum()
+//    println("The sum of number is $sum ")
+//    val avg = sum/num
+//    println("The Average of total number is : $avg")
+
+//    println("Enter the starting number : ")
+//    val start = readln().toInt()
+//    println("Enter the Ending Number : ")
+//    val end = readln().toInt()
+//    for(i in start until end){
+//        if (i%5 == 0) {
+//            println(i)
+//        }
+//    }
+    // println("Enter the Santanse here : ")
+    // val stn = readln()
+    // println("Enter the word you want to remove : ")
+    // val rword = readln()
+
+    // val replaceword = "-".repeat(rword.length)
+    // val result = stn.replace(rword, replaceword)
+    // println("Your Sentance without replacement is : $stn")
+    // println("Your Replacement word is : $rword")
+    // println("Your Final Sentance with replacement is : $result")
+}
